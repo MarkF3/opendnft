2cdc5840765e005d0ff92cef351a4d8fdbbd38ea
Mark Farrell <Markpeterfarrell@gmail.com>
2023-06-30 08:14:00 +0100

    NFT front-end style change

--- src/opend_frontend/src/components/Gallery.jsx
+++ src/opend_frontend/src/components/Gallery.jsx
@@ -27,8 +27,9 @@ setItems(props.ids.map((NFTId) => (
       <h3 className="makeStyles-title-99 Typography-h3">{props.title}</h3>
       <div className="disGrid-root disGrid-container disGrid-spacing-xs-2">
         <div className="disGrid-root disGrid-item disGrid-grid-xs-12">
-          <div className="disGrid-root disGrid-container disGrid-spacing-xs-5 disGrid-justify-content-xs-center"></div>
+          <div className="disGrid-root disGrid-container disGrid-spacing-xs-5 disGrid-justify-content-xs-center">
           {items}
+          </div>  
         </div>
       </div>
     </div>
